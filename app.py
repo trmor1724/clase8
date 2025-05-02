e66d551391713d5e6d5482047a9cc5be68d2fee8
tremor1724 <148923646+trmor1724@users.noreply.github.com>
2025-05-01 21:33:37 -0500

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 st.title("Reconocimiento de Imágenes IA")
 #st.write("Versión de Python:", platform.python_version())
-image = Image.open('OIG5.jpg')
+image = Image.open('images.jpg')
 st.image(image, width=350)
 with st.sidebar:
     st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar")
